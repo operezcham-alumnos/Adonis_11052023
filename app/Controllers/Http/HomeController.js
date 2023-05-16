e5aab57771ea5c14ac6af246282bb4656bb76962
Oscar Ernesto Pérez Cham <127453444+operezcham-alumnos@users.noreply.github.com>
2023-05-15 18:36:23 -0600

    git 🆕

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,11 +1,19 @@
+const { Octokit } = require("@octokit/rest");
+
 class HomeController {
-    async index ({ auth, view }) {
-      const user = await auth.getUser()
-  
-      return view.render('welcome', {
-        user: user || null
-      })
-    }
+  async index({ auth, view }) {
+    const user = await auth.getUser()
+
+    return view.render('welcome', {
+      user: user || null
+    })
   }
-  
-  module.exports = HomeController
+
+  async interaccion({ }) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
+    });
+  }
+}
+
+module.exports = HomeController
