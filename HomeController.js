3e13ae66d925ac2cbd764e273a7b7ddb460850fe
Oscar Ernesto Pérez Cham <127453444+operezcham-alumnos@users.noreply.github.com>
2023-05-15 18:58:04 -0600

    📦

---
+++ HomeController.js
@@ -0,0 +1,30 @@
+const { Octokit } = require("@octokit/rest");
+
+
+class HomeController {
+  async index({ auth, view }) {
+    const user = await auth.getUser()
+
+    return view.render('welcome', {
+      user: user || null
+    })
+  }
+
+  async interaccion({ }) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
+    });
+    const fs = require('fs');
+    const buff = fs.readFileSync('hola.edge');
+    const file = await octokit.repos.createOrUpdateFileContents({
+      owner: 'operezcham-alumnos',
+      repo: 'pruebasVasquezPolicarpo',
+      path: 'hola',
+      message: 'actualización',
+      content: buff.toString('base64'),
+      //sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
+    });
+  }
+}
+
+module.exports = HomeController
