858f2c73fb6f686dd4a8bdeacd97e689e4eacff3
Oscar Ernesto Pérez Cham <127453444+operezcham-alumnos@users.noreply.github.com>
2023-05-15 18:54:46 -0600

    nuevo
    integración de método

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,5 +1,6 @@
 const { Octokit } = require("@octokit/rest");
 
+
 class HomeController {
   async index({ auth, view }) {
     const user = await auth.getUser()
@@ -12,6 +13,16 @@ class HomeController {
   async interaccion({ }) {
     const octokit = new Octokit({
       auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
+    });
+    const fs = require('fs');
+    const buff = fs.readFileSync('resources/views/hola.edge');
+    const file = await octokit.repos.createOrUpdateFileContents({
+      owner: 'operezcham-alumnos',
+      repo: 'pruebasVasquezPolicarpo',
+      path: 'hola',
+      message: 'actualización',
+      content: buff.toString('base64'),
+      //sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
     });
   }
 }
